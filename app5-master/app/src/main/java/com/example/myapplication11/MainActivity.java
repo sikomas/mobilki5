10bab600bffbdf230f07f919501fc6353d1d8bff
Stepan Sikomas <stepan.sikomas@gmail.com>
2024-05-18 09:02:10 +0300

    first commit

---
+++ app5-master/app/src/main/java/com/example/myapplication11/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.myapplication11;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
+import androidx.core.app.NotificationCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import android.view.View;
+import com.example.myapplication11.fragments.StartFragment;
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
